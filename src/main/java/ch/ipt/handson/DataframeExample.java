e326eceb635f75f0b2ed4c65047e790c4c887b41
abhimanyupatel <abhimanyu.patel1991@gmail.com>
2019-09-23 19:36:00 +0200

    Added more example queries and options to test

--- src/main/java/ch/ipt/handson/DataframeExample.java
+++ src/main/java/ch/ipt/handson/DataframeExample.java
@@ -1,17 +1,18 @@
 package ch.ipt.handson;
 
 import org.apache.spark.SparkContext;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-import java.io.InputStream;
-import java.net.URL;
-
-import static org.apache.spark.sql.functions.desc;
+import static org.apache.spark.sql.functions.*;
+import static org.apache.spark.sql.functions.col;
 
 public class DataframeExample {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        final int DEFAULT_NUM_ROWS = 5;
+
         // Create a new local SparkSession
         SparkSession spark =
                 SparkSession.builder()
@@ -24,8 +25,48 @@ public class DataframeExample {
 
         // Read the online-retail-dataset.csv
         String path = "src/main/resources/online-retail-dataset.csv";
-        Dataset<Row> ds = spark.read().format("csv").option("header", "true").load(path);
 
+        // read all the data
+        Dataset<Row> ds = spark.read().format("csv")
+                .option("sep", ",")
+                .option("inferSchema", "true")
+                .option("header", "true")
+                .load(path);
+
+
+        ds.printSchema();
+        ds.show(2 * DEFAULT_NUM_ROWS);
+
+        // items per invoice
         ds.groupBy("InvoiceNo").count().sort(desc("count")).show();
+
+        // convert string column to timestamp column
+        Column invoiceDateColumn = to_timestamp(col("invoiceDate"), "MM/dd/yyyy HH:mm");
+        ds = ds.withColumn("invoiceDate", invoiceDateColumn);
+
+        Dataset<Row> purchasePerCustomerPerDay = ds.selectExpr("CustomerID",
+                "(UnitPrice * Quantity) as total_cost",
+                "InvoiceDate")
+                .groupBy(col("CustomerID"), window(col("InvoiceDate"), "1 day"))
+                .sum("total_cost")
+                .sort(desc("sum(total_cost)"))
+                .withColumn("sum(total_cost)", round(col("sum(total_cost)"), 2));
+//                .withColumnRenamed("sum(total_cost)", "spent")
+//                .withColumnRenamed("window", "timeframe");
+
+        purchasePerCustomerPerDay.explain();
+
+        purchasePerCustomerPerDay = purchasePerCustomerPerDay.na().drop();
+        purchasePerCustomerPerDay.show(DEFAULT_NUM_ROWS, false);
+
+        Dataset<Row> cleanedDataset = ds
+                .na().fill(0L)
+                .withColumn("DayOfWeek", date_format(col("InvoiceDate"), "EEEE"))
+                .repartition(4);
+        System.out.println("Number of partitions: " + cleanedDataset.rdd().partitions().length);
+        cleanedDataset.show(DEFAULT_NUM_ROWS);
+
+        // These could also be discarded - depends on whether we think this is valuable
+        cleanedDataset.where("CustomerID == 0").show(DEFAULT_NUM_ROWS);
     }
 }
--- src/main/java/ch/ipt/handson/SimpleLocalExample.java
+++ src/main/java/ch/ipt/handson/SimpleLocalExample.java
@@ -5,7 +5,7 @@ import org.apache.spark.sql.SparkSession;
 
 public class SimpleLocalExample {
 
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         // Create a new local SparkSession
         SparkSession spark =
                 SparkSession.builder()
@@ -17,7 +17,7 @@ public class SimpleLocalExample {
         context.setLogLevel("ERROR");
 
         // count on spark
-        long count = spark.range(1,2000).count();
+        long count = spark.range(1, 2000).count();
         System.out.println(count);
     }
 }
